f56ebf34d753e3fc98ff934b8c4f45b54100c657
Sayaz06 <adha86614@gmail.com>
2026-01-14 07:15:29 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,65 @@
+// Nama cache
+const CACHE_NAME = 'hafalan-retyping-v1';
+
+// Fail yang diprecache (shell app)
+const PRECACHE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Install event: cache fail asas
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activate event: buang cache lama
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE_NAME) {
+            return caches.delete(k);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event: network-first untuk dokumen, cache-first untuk aset
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  if (request.destination === 'document') {
+    // Network-first untuk HTML
+    event.respondWith(
+      fetch(request)
+        .then((res) => {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((c) => c.put(request, clone));
+          return res;
+        })
+        .catch(() => caches.match(request))
+    );
+  } else {
+    // Cache-first untuk CSS, JS, ikon, dll
+    event.respondWith(
+      caches.match(request).then((cached) => {
+        if (cached) return cached;
+        return fetch(request).then((res) => {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((c) => c.put(request, clone));
+          return res;
+        });
+      })
+    );
+  }
+});
