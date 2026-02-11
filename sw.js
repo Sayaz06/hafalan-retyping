70e0003856e58c9f464b62a488b7dd43a888daaa
Sayaz06 <adha86614@gmail.com>
2026-02-11 21:06:47 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nama cache
-const CACHE_NAME = 'hafalan-retyping-v13';
+const CACHE_NAME = 'hafalan-retyping-v14';
 
 // Fail yang diprecache (shell app) — ikut base path GitHub Pages
 const PRECACHE = [
